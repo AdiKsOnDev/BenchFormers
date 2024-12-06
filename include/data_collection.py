c6d99ba194edb28e4df464024714e4b29e54e330
AdiKsOnDev <adilalizade13@gmail.com>
2024-12-07 02:34:01 +0400

    feat: Made a function for creating a .csv with data

---
+++ include/data_collection.py
@@ -0,0 +1,43 @@
+import os
+import json
+import pandas as pd
+
+
+def collect_data(folders, output_csv="./data/dataset.csv"):
+    """
+    Iterates through JSON files in a folder, extracts 'main_body' and 'type'
+    attributes, and adds them to a pandas DataFrame, which gets saved in a CSV
+
+    Args:
+        folders (str): Path to the folder containing JSON files.
+        output_csv (str): Path to the output CSV file
+
+    Returns:
+        pd.DataFrame: A DataFrame with columns 'main_body' and 'type'.
+    """
+    data = []
+
+    for folder in folders:
+        for filename in os.listdir(folder):
+            if filename.endswith('.json'):
+                file_path = os.path.join(folder, filename)
+                try:
+                    with open(file_path, 'r') as file:
+                        json_data = json.load(file)
+
+                        main_body = json_data.get('main_body', None)
+                        doc_type = json_data.get('type', None)
+
+                        data.append({'main_body': main_body, 'type': doc_type})
+                except (json.JSONDecodeError, FileNotFoundError) as e:
+                    print(f"Error processing {filename}: {e}")
+
+    df = pd.DataFrame(data, columns=['main_body', 'type'])
+    df.to_csv(output_csv)
+
+    return df
+
+
+if __name__ == "__main__":
+    collect_data(folders=["./data/dataset/dataset/train/",
+                 "./data/dataset/dataset/test"])
