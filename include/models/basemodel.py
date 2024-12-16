b181559339b2a558b4d5ff70bf252891d8905675
AdiKsOnDev <adilalizade13@gmail.com>
2024-12-16 17:00:35 +0400

    chore: Added libraries necessary for fine-tuning

--- include/models/basemodel.py
+++ include/models/basemodel.py
@@ -2,9 +2,10 @@ from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
 
 class BaseModel:
-    def __init__(self, model_name, num_labels=2):
+    def __init__(self, model_name, num_labels=2, max_length=512):
         self.model_name = model_name
         self.num_labels = num_labels
+        self.max_length = max_length
         self._load_model(model_name, num_labels)
 
     def _load_model(self, model_name, num_labels):
@@ -12,10 +13,10 @@ class BaseModel:
         self.model = AutoModelForSequenceClassification.from_pretrained(
             model_name, num_labels=num_labels)
 
-    def tokenize(self, texts, max_length):
+    def tokenize(self, texts):
         return self.tokenizer(
             texts,
-            max_length=max_length,
+            max_length=self.max_length,
             padding="max_length",
             truncation=True,
             return_tensors="pt"
