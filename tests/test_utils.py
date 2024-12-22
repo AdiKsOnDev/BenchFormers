42f4410ec06892b8c83944b954291264cc884774
AdiKsOnDev <adilalizade13@gmail.com>
2024-12-22 21:08:19 +0400

    test: Added a test for dataset shuffler

---
+++ tests/test_utils.py
@@ -0,0 +1,15 @@
+import unittest
+import pandas as pd
+
+from include.utils import limit_dataset
+
+
+class TestModels(unittest.TestCase):
+    def setUp(self):
+        self.df = pd.DataFrame(
+            {'A': [1, 2, 3, 4, 5], 'B': [10, 20, 30, 40, 50]})
+
+    def test_limit_dataset(self):
+        result = limit_dataset(self.df, 3)
+
+        self.assertEqual(3, result.shape[0])
